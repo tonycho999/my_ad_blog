cbcf7535452830210d69e14d28e1269f05cb4af1
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-05-06 07:51:19 +0800

    Rename blog title from 'My Ad Blog' to 'My Blog'

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="container mx-auto flex justify-between items-center px-4">
         {/* 블로그 로고는 SEO를 위해 남겨두되, 다른 링크는 제거합니다. */}
         <Link href="/" className="text-2xl font-bold tracking-tight text-gray-900">
-          My Ad Blog
+          My Blog
         </Link>
         
         {/* 네비게이션 메뉴 삭제됨 */}
